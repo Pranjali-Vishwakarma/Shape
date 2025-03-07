4ab11817cd0dddbf0953009591985506994b9c87
Pranjali Vishwakarma <141571484+Pranjali-Vishwakarma@users.noreply.github.com>
2025-03-07 23:09:10 +0530

    Add files via upload

---
+++ Sphere.java
@@ -0,0 +1,18 @@
+// Sphere.java
+class Sphere extends Shape implements Volume {
+    public Sphere(double radius) {
+        super(radius, 0, 0, 0);
+    }
+
+    public double calculateArea() {
+        return 4 * Math.PI * dim_one * dim_one;
+    }
+
+    public double calculatePerimeter() {
+        return 0; // No perimeter for a sphere
+    }
+
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(dim_one, 3);
+    }
+}

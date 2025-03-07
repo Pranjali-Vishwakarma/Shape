0fe27874f6ff5e80d892a0252a6efad4b956022d
Pranjali Vishwakarma <141571484+Pranjali-Vishwakarma@users.noreply.github.com>
2025-03-07 23:07:00 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,14 @@
+// Circle.java
+class Circle extends Shape {
+    public Circle(double radius) {
+        super(radius, 0, 0, 0);
+    }
+
+    public double calculateArea() {
+        return Math.PI * dim_one * dim_one;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * dim_one;
+    }
+}
